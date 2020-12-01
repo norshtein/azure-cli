b231ad64a238b109c5be9ae8942ba2ae699fcc72
Tongyao Si <tosi@microsoft.com>
2020-12-01 11:31:34 +0000

    Use python3

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git clone https://github.com/norshtein/azure-cli.git
 cd azure-cli
 git checkout tosi/default-msi
 
-python -m venv env
+python3 -m venv env
 source env/bin/activate
 
 pip install azdev
